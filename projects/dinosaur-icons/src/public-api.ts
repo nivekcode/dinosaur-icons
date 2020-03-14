1fb4340ca6fdb6a9a74209ffbe51f5264448032e
kreuzerk <kevin.kreuzer90@icloud.com>
2020-03-14 09:25:29 +0100

    chore(icons): export registry service

--- projects/dinosaur-icons/src/public-api.ts
+++ projects/dinosaur-icons/src/public-api.ts
@@ -1,7 +1,7 @@
 /*
  * Public API Surface of dinosaur-icons
  */
-
-export * from './lib/dinosaur-icons.service';
+export * from './lib/dinosaur-icons-registry.service';
 export * from './lib/dinosaur-icons.component';
+export * from './lib/dinosaur-icons';
 export * from './lib/dinosaur-icons.module';
